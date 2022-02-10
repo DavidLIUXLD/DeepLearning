3780a8235c4d4edd78e32129520d5d0526966cc9
DavidLIUXLD <47655919+DavidLIUXLD@users.noreply.github.com>
2022-02-09 21:04:22 -0500

    Update ss.py

--- model/ss.py
+++ model/ss.py
@@ -2,22 +2,22 @@ import os
 from subprocess import *
 
 #run secondary script 1
-p = Popen([r'Python', 'C:\\Users\\David\\OneDrive\\Desktop\\UF\\Academics\\Spring 2022\\CAP 4613\\project\\p1\\model\\knnReg.py'], shell=True, stdin=PIPE, stdout=PIPE)
+p = Popen([r'Python', 'model\\knnReg.py'], shell=True, stdin=PIPE, stdout=PIPE)
 output = p.communicate()
 print(output[0])
 
 #run secondary script 2
-p = Popen([r'Python', 'C:\\Users\\David\\OneDrive\\Desktop\\UF\\Academics\\Spring 2022\\CAP 4613\\project\\p1\\model\\mlpReg.py'], shell=True, stdin=PIPE, stdout=PIPE)
+p = Popen([r'Python', 'model\\mlpReg.py'], shell=True, stdin=PIPE, stdout=PIPE)
 output = p.communicate()
 
-p = Popen([r'Python', 'C:\\Users\\David\\OneDrive\\Desktop\\UF\\Academics\\Spring 2022\\CAP 4613\\project\\p1\\model\\lrReg.py'], shell=True, stdin=PIPE, stdout=PIPE)
+p = Popen([r'Python', 'model\\lrReg.py'], shell=True, stdin=PIPE, stdout=PIPE)
 output = p.communicate()
 
-p = Popen([r'Python', 'C:\\Users\\David\\OneDrive\\Desktop\\UF\\Academics\\Spring 2022\\CAP 4613\\project\\p1\\model\\svmclassif.py'], shell=True, stdin=PIPE, stdout=PIPE)
+p = Popen([r'Python', 'model\\svmclassif.py'], shell=True, stdin=PIPE, stdout=PIPE)
 output = p.communicate()
 
-p = Popen([r'Python', 'C:\\Users\\David\\OneDrive\\Desktop\\UF\\Academics\\Spring 2022\\CAP 4613\\project\\p1\\model\\knnclassif.py'], shell=True, stdin=PIPE, stdout=PIPE)
+p = Popen([r'Python', 'model\\knnclassif.py'], shell=True, stdin=PIPE, stdout=PIPE)
 output = p.communicate()
 
-p = Popen([r'Python', 'C:\\Users\\David\\OneDrive\\Desktop\\UF\\Academics\\Spring 2022\\CAP 4613\\project\\p1\\model\\mlpclassif.py'], shell=True, stdin=PIPE, stdout=PIPE)
+p = Popen([r'Python', 'model\\mlpclassif.py'], shell=True, stdin=PIPE, stdout=PIPE)
 output = p.communicate()
